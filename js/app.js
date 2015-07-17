61da2b3cffc343110188671f34b1687569ba4773
terrygriffin <jtgrffn@gmail.com>
2015-07-16 20:17:38 -0500

    routes incomplete

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
 
+'use strict';
+
 // 'ngRoute' defines an app dependency: prepare to route templates.
-var app = angular.module('MyStuffApp', []);
+var app = angular.module('MyStuffApp', [
+	'ngRoute',
+	'MainController'
+	]);
 
 //'$routeProvider' defines the template routes.
 // '/' is the root URL, i.e. index.html. Default. 
-// app.config(['$routeProvider', function($routeProvider) {
-// 		$routeProvider
-// 			.when('/', {
-// 				controller: 'MainController',
-// 				templateUrl: '/templates/home.html'
-// 			})
-// 			.otherwise({
-// 				redirectTo: '/'
-// 			});
-// 	}
-// ]);
+app.config(['$routeProvider', 
+	function($routeProvider) {
+		$routeProvider
+			.when('/instruments', {
+				controller: 'instmtDataCtrl',
+				templateUrl: '/templates/list-instrument.html'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+	}
+]);
--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,54 +1,18 @@
-app.controller('MainController', ['$scope', '$http',
+
+var mainController = angular.module('MainController', []);
+
+mainController.controller('instmtDataCtrl', ['$scope', '$http',
 	function($scope, $http) {
-
-
 		$scope.title = 'Instruments';
-
-
 		$http.get('data/instruments.json').success(function(data){
 			// return all data from the JSON file, and spit out only the first two objects.
 			$scope.instruments = data;
 		});
-
 		$scope.listOrder = 'name';
-
-
-
-
-		// $scope.instruments = [
-
-		// 	{
-		// 		type: 'Guitar',
-		// 		name: 'Larrivee',
-		// 		location: 'Durham, NC',
-		// 		condition: 'New',
-		// 		age: 3,
-		// 		image: 'img/'
-		// 	},
-		// 	{
-		// 		type: 'Violin',
-		// 		name: 'Fiddle',
-		// 		location: 'Durham, NC',
-		// 		condition: 'Antique',
-		// 		age: 100,
-		// 		image: 'img/'
-		// 	},
-		// 	{
-		// 		type: 'Banjo',
-		// 		name: 'Rogue',
-		// 		location: 'Madison, WI',
-		// 		condition: 'Second-hand',
-		// 		age: 10,
-		// 		image: 'img/'
-		// 	},
-		// 	{
-		// 		type: 'Drum',
-		// 		name: 'Big Snare Drum',
-		// 		location: 'Durham, NC',
-		// 		condition: 'Modified',
-		// 		age: 10,
-		// 		image: 'img/'
-		// 	}
-		// ];
 	}
 ]);
+// mainController.controller('instmtDetailCtrl', ['$scope', '$routeParams', 
+// 	function($scope, $routeParams) {
+// 		$scope.instrumentId = $routeParams.instrumentId;
+// 	}
+// ]);
